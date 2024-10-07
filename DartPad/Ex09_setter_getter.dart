1478834ce604be6a81f44f3cd64da13da55e537a
suripel1 <suripeli04@gmail.com>
2024-10-07 17:54:26 -0600

    Practica 12
    Ejercicio 10: Agregacion de aserciones

--- DartPad/Ex09_setter_getter.dart
+++ DartPad/Ex09_setter_getter.dart
@@ -1,27 +1,40 @@
-void main(){
-  final mySquare = Square(side:10);
+void main() {
+    final mySquare = Square(side: 18);
 
-  mySquare.side = 5;
-  print("El area es igual a: ${mySquare.area}");
+    print("""
+          Lado del Cuadrado: ${mySquare.side}
+          Área que ocupa: ${mySquare.calculateArea()}""");
+
+    mySquare.side = -32;
+
 }
-
 class Square {
-  double _side;
-  
-  Square({ required double side })
+    double _side;
+
+
+    Square({required double side})
     : _side = side;
 
-  double get area {
-      return _side * _side;
-  }
 
-  set side(double value){
-    print("setting new value $value");
-      if (value < 0) throw 'Value must be >=0';
-    _side = value;
-  }
-  
-  double calculateArea() {
-  return _side * _side;
-  }
+    double get area {
+        return _side * _side;
+    }
+
+    double get side {
+        return _side;
+    }
+
+
+    set side(double value) {
+        print("Asignando un nuevo valor al lado: $value");
+
+        if (value < 0) throw 'El valor del lado debe ser mayor a 0.';
+
+        _side = value;
+    }
+
+
+    double calculateArea() {
+        return _side * _side;
+    }
 }
